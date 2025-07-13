ab554dbe623e7e2461538e2aab7a472aec5cc5a3
JEONGMOON <k8852055@gmail.com>
2025-07-13 21:59:21 +0900

    first commit

--- task2/power_calculator.py
+++ task2/power_calculator.py
@@ -26,3 +26,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+# 0의 0제곱
---
+++ task5/sort_calculator.py
@@ -0,0 +1,16 @@
+def main():
+    numbers = list(map(float, input("Enter numbers: ").split()))
+
+    for i in range(len(numbers)):
+        j = len(numbers) - i
+        for k in range(1,j):
+            if numbers[k-1] >= numbers[k]:
+                temp = numbers[k-1]
+                numbers[k-1] = numbers[k]
+                numbers[k] = temp
+
+    formatted = ''.join(f"<{x}>" for x in numbers)
+    print(f"Sorted: {formatted}")
+
+if __name__ == "__main__":
+    main()   
