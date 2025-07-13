c9aa76fcaf9a3c472c68e38e99a22a00a7b1195f
JEONGMOON <k8852055@gmail.com>
2025-07-13 22:37:15 +0900

    second commit

--- task5/sort_calculator.py
+++ task5/sort_calculator.py
@@ -1,16 +1,22 @@
 def main():
-    numbers = list(map(float, input("Enter numbers: ").split()))
-
-    for i in range(len(numbers)):
-        j = len(numbers) - i
+    try:
+        numbers = list(map(float, input("Enter numbers: ").split()))
+        
+        for i in range(len(numbers)):
+            j = len(numbers) - i
         for k in range(1,j):
             if numbers[k-1] >= numbers[k]:
                 temp = numbers[k-1]
                 numbers[k-1] = numbers[k]
                 numbers[k] = temp
-
+        
+    except ValueError:
+        print("Invalid number input.")
+        return
+    
     formatted = ''.join(f"<{x}>" for x in numbers)
     print(f"Sorted: {formatted}")
+    
 
 if __name__ == "__main__":
     main()   
