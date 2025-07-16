a90c6a04bd186eacb1890846b1fac4c6a52cb814
JEONGMOON <k8852055@gmail.com>
2025-07-16 13:58:38 +0900

    commit

--- task3/david/calculator.py
+++ task3/david/calculator.py
@@ -7,49 +7,52 @@ def subtract(a, b):
 def multiply(a, b):
     return a * b
 
-def divide(a, b):
-    return a / b
+def divide(a, b): #assert 사용해서도 작성해보기. (except ZeroDivisionError: 삽입)
+    if b == 0:
+        print("Error: Division by zero.")
+        return None
+    else:
+        return a / b
 
 
-def main():
-    assert(1 < 0)
+def first_calculator():
 
-    # try:
-    #     a = int(input("Enter num1: "))
-    # except ValueError:
-    #     print("Invalid number input.")
-    #     return
+    try:
+        a = int(input("Enter num1: "))
+    except ValueError:
+        print("Invalid number input.")
+        return
 
-    # try:
-    #     b = int(input("Enter num2: "))
-    # except ValueError:
-    #     print("Invalid number input.")
-    #     return
+    try:
+        b = int(input("Enter num2: "))
+    except ValueError:
+        print("Invalid number input.")
+        return
+    
+    operator = input("Enter operator: ")
 
-    # operator = input("Enter operator: ")
+    if operator == "+":
+        result = add(a, b)
+    elif operator == "-":
+        result = subtract(a, b)
+    elif operator == "*":
+        result = multiply(a, b)
+    elif operator == "/":
+        result = divide(a, b)
+        if result is None:
+            return
+    else:
+        print("Invalid operator.")
+        return
 
-    # if operator == "+":
-    #     result = add(a, b)
-    # elif operator == "-":
-    #     result = subtract(a, b)
-    # elif operator == "*":
-    #     result = multiply(a, b)
-    # elif operator == "/":
-    #     result = divide(a, b)
-    #     if result is None:
-    #         return
-    # else:
-    #     print("Invalid operator.")
-    #     return
-
-    # print(f"Result: <{result}>")
+    print(f"Result: <{result}>")
     
 
-
+def second_calculator():
     enter_expression = input("Enter expression: ")
 
     try:
-        parts = enter_expression.strip().split()
+        parts = enter_expression.split() #strip() 필요없음.
         # parts = enter_expression.strip().split(maxsplit=2)
         if len(parts) != 3:
             print("Invalid expression.")
@@ -67,6 +70,8 @@ def main():
             result = multiply(a, b)
         elif operator == "/":
             result = divide(a, b)
+            if result is None:
+                return
         else:
             print("Invalid expression.")
             return
@@ -75,9 +80,15 @@ def main():
     
     except ValueError:
         print("Invalid expression.")
-    except ZeroDivisionError:
-        print("Error: Division by zero.")
         
+def main():
+    mode = input("Press the number(1 or 2): ")
+    if mode == "1":
+        first_calculator()
+    elif mode == "2":
+        second_calculator()
+    else:
+        print("Invalid expression.")
 
 if __name__ == "__main__":
     main()
--- task5/sort_calculator.py
+++ task5/sort_calculator.py
@@ -1,19 +1,18 @@
 def main():
     try:
         numbers = list(map(float, input("Enter numbers: ").split()))
-        
-        for i in range(len(numbers)):
-            j = len(numbers) - i
-        for k in range(1,j):
-            if numbers[k-1] >= numbers[k]:
-                temp = numbers[k-1]
-                numbers[k-1] = numbers[k]
-                numbers[k] = temp
-        
     except ValueError:
         print("Invalid number input.")
         return
     
+    for i in range(len(numbers)):
+            j = len(numbers) - i
+            for k in range(1,j):
+                if numbers[k-1] >= numbers[k]:
+                    temp = numbers[k-1]
+                    numbers[k-1] = numbers[k]
+                    numbers[k] = temp
+
     formatted = ''.join(f"<{x}>" for x in numbers)
     print(f"Sorted: {formatted}")
     
